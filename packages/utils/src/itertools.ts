d64875ff4d4b3d803415b2010d2b851942871257
Sebastian Aranguiz <sarang575@gmail.com>
2021-10-25 01:24:21 -0300

    Set `noUncheckedIndexedAccess` on utils

--- packages/utils/src/itertools.ts
+++ packages/utils/src/itertools.ts
@@ -3,9 +3,9 @@ export const chunk = <T>(input: T[], size: number): T[][] =>
   input.reduce<T[][]>((arr, item, idx) => {
     return idx % size === 0
       ? [...arr, [item]]
-      : [...arr.slice(0, -1), [...arr.slice(-1)[0], item]]
+      : [...arr.slice(0, -1), [...(arr.slice(-1)[0] ?? []), item]]
   }, [])
 
 // https://stackoverflow.com/a/10284006
 export const zip = <T>(rows: T[][]): T[][] =>
-  rows[0].map((_, c) => rows.map((row) => row[c]))
+  rows[0]?.map((_, c) => rows.map((row) => row[c]!)) ?? []
