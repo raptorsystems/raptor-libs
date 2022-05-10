96e28c101e4820255cee23726b3566951e1554db
Sebastian Aranguiz <sarang575@gmail.com>
2022-05-10 05:18:20 -0400

    Add apiRequestContext & sentryRequestContext plugins

--- packages/graphql-server/src/plugins/index.ts
+++ packages/graphql-server/src/plugins/index.ts
@@ -1,3 +1,3 @@
 export { auth0Plugin } from './auth0'
 export { errorHandlerPlugin } from './errorHandler'
-export { requestContextPlugin } from './requestContext'
+export { apiRequestContext, sentryRequestContext } from './requestContext'
--- packages/graphql-server/src/plugins/requestContext.ts
+++ packages/graphql-server/src/plugins/requestContext.ts
@@ -1,26 +1,59 @@
-import { asyncResourceSymbol, getAsyncStoreInstance } from '@raptor/graphql-api'
-import { AsyncResource } from 'async_hooks'
+import { AsyncLocalStorage, AsyncResource } from 'async_hooks'
 import type { FastifyPluginCallback } from 'fastify'
+import { getAsyncStoreInstance } from '@raptor/graphql-api'
+import { getSentryHubAsyncStoreInstance, makeSentryHub } from '@raptor/sentry'
 import fp from 'fastify-plugin'
 
-const requestContext: FastifyPluginCallback = (instance, _opts, next) => {
-  // Setup request-scoped context, based on Async hooks
-  // ref: https://github.com/fastify/fastify-request-context
-  void instance.addHook('onRequest', (req, _res, done) => {
-    const localAsyncStorage = getAsyncStoreInstance()
-    localAsyncStorage.run(new Map(), () => {
-      const asyncResource = new AsyncResource('request-context')
-      req[asyncResourceSymbol] = asyncResource
-      asyncResource.runInAsyncScope(done, req.raw)
-    })
-  })
+type GetAsyncStore<T> = () => AsyncLocalStorage<T>
 
-  void instance.addHook('preValidation', (req, _res, done) => {
-    const asyncResource = req[asyncResourceSymbol] as AsyncResource
-    asyncResource.runInAsyncScope(done, req.raw)
-  })
-
-  next()
+export interface AsyncContextOptions<T> {
+  defaultStoreValues: T
+  asyncResourceKey: string
 }
 
-export const requestContextPlugin = fp(requestContext)
+const requestContext =
+  <T>({
+    asyncStore,
+    defaultStoreValues,
+    asyncResourceKey,
+  }: {
+    asyncStore: GetAsyncStore<T>
+  } & AsyncContextOptions<T>): FastifyPluginCallback<AsyncContextOptions<T>> =>
+  (instance, opts, done) => {
+    const asyncResourceSymbol = Symbol(
+      opts.asyncResourceKey ?? asyncResourceKey,
+    )
+
+    // Setup request-scoped context, based on Async hooks
+    // ref: https://github.com/fastify/fastify-request-context
+    void instance.addHook('onRequest', (req, _res, done) => {
+      asyncStore().run(opts.defaultStoreValues ?? defaultStoreValues, () => {
+        const asyncResource = new AsyncResource(asyncResourceKey)
+        req[asyncResourceSymbol] = asyncResource
+        asyncResource.runInAsyncScope(done, req.raw)
+      })
+    })
+
+    void instance.addHook('preValidation', (req, _res, done) => {
+      const asyncResource = req[asyncResourceSymbol] as AsyncResource
+      asyncResource.runInAsyncScope(done, req.raw)
+    })
+
+    done()
+  }
+
+export const apiRequestContext = fp(
+  requestContext({
+    asyncStore: getAsyncStoreInstance,
+    defaultStoreValues: new Map(),
+    asyncResourceKey: 'apiContext',
+  }),
+)
+
+export const sentryRequestContext = fp(
+  requestContext({
+    asyncStore: getSentryHubAsyncStoreInstance,
+    defaultStoreValues: makeSentryHub(),
+    asyncResourceKey: 'sentryHubContext',
+  }),
+)
