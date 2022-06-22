5497e921b1e8f181ad9e565c88617352ed919cc6
Sebastian Aranguiz <sarang575@gmail.com>
2022-06-21 20:36:31 -0400

    Update fastify to v4

--- packages/graphql-server/src/plugins/errorHandler.ts
+++ packages/graphql-server/src/plugins/errorHandler.ts
@@ -3,14 +3,11 @@ import type { FastifyPluginCallback } from 'fastify'
 import fp from 'fastify-plugin'
 
 const errorHandler: FastifyPluginCallback = (instance, _opts, done) => {
+  const defaultErrorHandler = instance.errorHandler
+
   instance.setErrorHandler((error, request, reply) => {
     Sentry.captureException(error)
-    // default handler
-    if (reply.statusCode < 500)
-      reply.log.info({ res: reply, err: error }, error && error.message)
-    else
-      reply.log.error({ req: request, res: reply, err: error }, error?.message)
-    void reply.send(error)
+    defaultErrorHandler(error, request, reply)
   })
 
   done()
