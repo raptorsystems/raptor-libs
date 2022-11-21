14f42abbce18462fb92c19a1477b0399143cddf6
Sebastian Aranguiz <sarang575@gmail.com>
2022-11-20 21:12:49 -0300

    Separate graphql-api base and auth contexts

--- packages/graphql-api/src/context/index.ts
+++ packages/graphql-api/src/context/index.ts
@@ -1,21 +1,30 @@
+import { YogaInitialContext } from 'graphql-yoga'
 import { SentryService } from '../services/sentry.service'
 import { UserService } from '../services/user.service'
 import { UserHeaders, UserPayload } from '../types'
 import type { GlobalContext } from './global'
 import * as global from './global'
 
-export interface BaseContext<TUserService extends UserService = UserService>
-  extends GlobalContext {
+export type BaseContext = GlobalContext
+
+export interface AuthContext<TUserService extends UserService = UserService>
+  extends BaseContext {
   user: TUserService
   sentry: SentryService
 }
 
-export type ContextFactory<Context extends BaseContext = BaseContext> = (
-  token: string,
-  payload: UserPayload,
-  headers: UserHeaders,
+export type AuthContextFactory<Context extends AuthContext = AuthContext> = (
+  initialContext: YogaInitialContext & {
+    token: string
+    payload: UserPayload
+    headers: UserHeaders
+  },
+) => Context
+
+export type BaseContextFactory<Context extends BaseContext = BaseContext> = (
+  initialContext: YogaInitialContext,
 ) => Context
 
 export * from './global'
 
-export const context = global.context as BaseContext
+export const context = global.context
