5aef86ec55ca10c49bdc32e3ab3e92f188dd658c
Sebastian Aranguiz <sarang575@gmail.com>
2023-03-11 17:33:49 -0300

    Use apollo HttpLink instead of @graphql-tools/executor-http

--- packages/nuxt-apollo/src/link.http.ts
+++ packages/nuxt-apollo/src/link.http.ts
@@ -1,6 +1,4 @@
-import { ApolloLink, Observable, RequestHandler } from '@apollo/client/core'
-import { buildHTTPExecutor } from '@graphql-tools/executor-http'
-import { Executor, isAsyncIterable } from '@graphql-tools/utils'
+import { HttpLink } from '@apollo/client/core'
 import type { CreateApolloHttpLink } from './types'
 import { axiosFetch } from './utils/axios'
 
@@ -23,43 +21,5 @@ export const createHttpLink: CreateApolloHttpLink = ({
 
   // Use buildHTTPExecutor with YogaLink as ref
   // https://github.com/dotansimha/graphql-yoga/blob/14bc965442b0b2b76b22b215c616ac27fe4dc455/packages/client/apollo-link/src/index.ts
-  return new ExecutorLink(buildHTTPExecutor({ endpoint, fetch }))
-}
-
-// Reimplement ExecutorLink
-// https://github.com/ardatan/graphql-tools/blob/c1a85885718f2cbc546a8a36506db55892565bf9/packages/executors/apollo-link/src/index.ts
-// Original implementation uses @apollo/client, which imports react. Reimplement using @apollo/client/core
-const createApolloRequestHandler =
-  (executor: Executor): RequestHandler =>
-  (operation) =>
-    new Observable((observer) => {
-      void Promise.resolve().then(async () => {
-        try {
-          const results = await executor({
-            document: operation.query,
-            variables: operation.variables,
-            operationName: operation.operationName,
-            extensions: operation.extensions,
-            context: operation.getContext(),
-          })
-          if (isAsyncIterable(results)) {
-            for await (const result of results) {
-              if (observer.closed) return
-              observer.next(result)
-            }
-            observer.complete()
-          } else if (!observer.closed) {
-            observer.next(results)
-            observer.complete()
-          }
-        } catch (error) {
-          if (!observer.closed) observer.error(error)
-        }
-      })
-    })
-
-export class ExecutorLink extends ApolloLink {
-  constructor(executor: Executor) {
-    super(createApolloRequestHandler(executor))
-  }
+  return new HttpLink({ uri: endpoint, fetch })
 }
