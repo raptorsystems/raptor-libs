ece9c1224f828c0688469f4dbfb278019d007ddb
Sebastian Aranguiz <sarang575@gmail.com>
2023-07-25 21:37:48 -0400

    Remove runServer function

--- packages/graphql-server/src/index.ts
+++ packages/graphql-server/src/index.ts
@@ -1,19 +1,2 @@
-import chalk from 'chalk'
-import consola from 'consola'
-import type { FastifyInstance, FastifyListenOptions } from 'fastify'
-
-export async function runServer(
-  server: FastifyInstance,
-  opts: FastifyListenOptions,
-) {
-  try {
-    const url = await server.listen(opts)
-    consola.info(chalk.bold(`Listening on: ${url}`))
-  } catch (error) {
-    server.log.fatal(error)
-    process.exit(1)
-  }
-}
-
 export * from './plugins'
 export * from './routes'
