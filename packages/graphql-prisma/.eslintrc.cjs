14a542ed562e9fdeca2ed560f8b2fdee1b074ea9
Sebastian Aranguiz <sarang575@gmail.com>
2024-05-19 15:40:02 -0400

    Remove graphql-prisma package

--- packages/graphql-prisma/.eslintrc.cjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('eslint').Linter.Config} */
-module.exports = {
-  extends: ['@raptor/eslint-config/type-checking'],
-  parserOptions: {
-    tsconfigRootDir: __dirname,
-    project: ['./tsconfig.json'],
-  },
-}
