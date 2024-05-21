bd2816655e1a8188771077556593a94bc96ed779
Sebastian Aranguiz <sarang575@gmail.com>
2024-05-21 02:39:53 -0400

    Remove yarn plugin packages

--- packages/yarn-plugins/packages/plugin-docker/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { Plugin } from '@yarnpkg/core'
-
-import pack from './commands/pack.ts'
-
-const plugin: Plugin = {
-  commands: [pack],
-}
-
-export default plugin
--- packages/yarn-plugins/packages/plugin-resolve/.eslintrc.cjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('eslint').Linter.Config} */
-module.exports = {
-  extends: ['@raptor/eslint-config/type-checking'],
-  ignorePatterns: ['bundles'],
-  parserOptions: {
-    tsconfigRootDir: __dirname,
-    project: ['./tsconfig.json'],
-  },
-}
