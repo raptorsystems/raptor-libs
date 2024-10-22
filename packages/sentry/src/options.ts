226d7fe171717b1bf4c715b828eaf7d96ca8cf75
Sebastian Aranguiz <sarang575@gmail.com>
2024-10-22 19:17:01 -0300

    Update sentry node to v8

--- packages/sentry/src/options.ts
+++ packages/sentry/src/options.ts
@@ -1,17 +1,20 @@
-import sentryIntegrations from '@sentry/integrations'
+import {
+  extraErrorDataIntegration,
+  rewriteFramesIntegration,
+} from '@sentry/node'
 import type { Options } from '@sentry/types'
 
 export const sentryDefaultOptions: Options = {
   // ? Set error depth
   // https://github.com/getsentry/sentry-javascript/issues/1964#issuecomment-625353135
-  integrations: [sentryIntegrations.extraErrorDataIntegration({ depth: 9 })],
+  integrations: [extraErrorDataIntegration({ depth: 9 })],
   normalizeDepth: 10, // depth + 1
 }
 
 export const sentryLambdaOptions: Options = {
   ...sentryDefaultOptions,
   integrations: [
-    sentryIntegrations.extraErrorDataIntegration({ depth: 9 }),
-    sentryIntegrations.rewriteFramesIntegration(),
+    extraErrorDataIntegration({ depth: 9 }),
+    rewriteFramesIntegration(),
   ],
 }
