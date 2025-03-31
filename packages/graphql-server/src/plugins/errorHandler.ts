d28acd747352627821d0e6e8ab5b5150ff343eda
Sebastian Aranguiz <sarang575@gmail.com>
2025-03-30 23:24:54 -0300

    Refactor Sentry context handling

--- packages/graphql-server/src/plugins/errorHandler.ts
+++ packages/graphql-server/src/plugins/errorHandler.ts
@@ -6,12 +6,11 @@ const errorHandler: FastifyPluginCallback = (instance, _opts, done) => {
   const defaultErrorHandler = instance.errorHandler
 
   instance.setErrorHandler((error, request, reply) => {
-    const scope = Sentry.getCurrentScope()
     if (instance.auth0) {
       try {
         const token = instance.auth0.getToken(request.headers)
         const user = instance.auth0.decodeToken(token)
-        scope.setUser({ id: user.sub })
+        Sentry.setUser({ id: user.sub })
       } catch (error) {
         instance.log.warn(error)
       }
